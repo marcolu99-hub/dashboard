854f4e27272bfff9a62c84c743ef8faa4346115c
marcolu99-hub <la-tua-email-di-github@example.com>
2025-08-27 02:25:02 +0200

    Collegato dashboard al Supabase del bot

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Inserisci le credenziali del tuo progetto Supabase
-const supabaseUrl = "https://bxamguzirhfpgbralfcw.supabase.co";   // Project URL dal sito Supabase
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4YW1ndXppcmhmcGdicmFsZmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyNDkzNzMsImV4cCI6MjA3MTgyNTM3M30.BoGDM-kPfhi9GaoCFns1_5dAetp6JAukIm-rSZ10Ldk";       // Chiave anon pubblica dal sito Supabase
+const supabaseUrl = "https://bynvngiuedwbiwziepmg.supabase.co";   // Project URL dal sito Supabase
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5bnZuZ2l1ZWR3Yml3emllcG1nIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyNDE1MDksImV4cCI6MjA3MTgxNzUwOX0.ZPAPBnV4nl4D3v8wGt1qSGooXTx4tP_TR-Jnyx7aNjQ";       // Chiave anon pubblica dal sito Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
